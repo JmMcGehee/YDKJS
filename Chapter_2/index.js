3589c904d0e73e53e4a8d5d8a2204636cfe705ca
Joshua McGehee <mcgehee.joshua@gmail.com>
2022-03-19 19:40:08 -0600

    variables

--- Chapter_2/index.js
+++ Chapter_2/index.js
@@ -1,2 +1,40 @@
-let a = null
-console.log(typeof a) // Prints "object"....weird
+// let a = null
+// console.log(typeof a) // Prints "object"....weird
+
+// // Q. What is datatype COERSION? 
+
+// // Built-in type Methods
+
+// let b = 'hello world'
+// console.log(b.toUpperCase()); // ==> 'HELLO WORLD'
+
+// // Q. We get built in methods from Object Wrappers (wrapper objects?). 
+// // Q. Why is there 'string' verses 'String'?
+
+// // Variable hoisting: 
+
+// let c = 2
+
+// foo()
+
+// function foo() {
+//     c = 3
+
+//     console.log(c)
+
+//     var c 
+// }
+
+// console.log(c);
+
+// Q. Can foo() run on line 18 even though it's declared on line 20 because of 'hoisting'?
+
+// Scope: Variables can only be accesses inside of the scope in which they are declared. 
+
+// function foo() {
+//     a = 3
+// }
+
+// foo();
+// console.log(a) // only works because 'a' automatically becomes a global variable
+// // ALWAYS FORMALLY DECLARE YOUR VARIABLES. 
